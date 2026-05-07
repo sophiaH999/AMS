2eacf23a49c4f5b91d9d6abbfcae8c57192b925e
sophiaH999 <sophiaharumi2009@icloud.com>
2026-05-07 15:28:48 -0300

    Add files via upload

--- PW1/Curriculo/JS/script.js
+++ PW1/Curriculo/JS/script.js
@@ -14,4 +14,22 @@ botao.addEventListener("click", ()=> {
 } else {
     localStorage.setItem("tema", "light");
 }
-});
+const botao= document.getElementById("tema");
+)};
+const updateButtonIcon=() =>{
+    if(!botao) return;
+    const darkMode= document.body.classList.constains("dark");
+    botsao.textContent= darMode ? "☀️" : "🌑";
+    botao.title= darkMode? "mudar para modo claro": "mudar para modo escuro";
+};
+
+const temaAtual= localStorage.getItem("tema")==="dark" ? "dark": "light";
+if (temaAtual==="dark"){
+    document.body.classList.add("dark");
+}
+
+updateButtonIcon();
+
+if(botao){
+    botao.addEventListener("click",())
+}
